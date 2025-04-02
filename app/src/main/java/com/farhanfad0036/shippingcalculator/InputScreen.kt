ab1428187848486eca8e28358d558eeb19cfc8ca
Muhammad Farhan Fadlillah <ffadlillah18@gmail.com>
2025-04-02 16:24:25 +0700

    menambahkan arrow back(up)

--- app/src/main/java/com/farhanfad0036/shippingcalculator/InputScreen.kt
+++ app/src/main/java/com/farhanfad0036/shippingcalculator/InputScreen.kt
@@ -1,15 +1,41 @@
 package com.farhanfad0036.shippingcalculator
 
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.compose.material3.*
+import androidx.compose.ui.platform.LocalContext
+import java.text.NumberFormat
+import java.util.Locale
+import android.content.Intent
+import android.content.Context
+import android.util.Log
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputScreen(navController: NavController) {
     var distance by rememberSaveable { mutableStateOf("") }
@@ -17,5 +43,17 @@ fun InputScreen(navController: NavController) {
     var selectedService by rememberSaveable { mutableStateOf("Regular") }
     var result by rememberSaveable { mutableStateOf("") }
 
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text("Shipping Calculator") },
+                navigationIcon = {
+                    IconButton(onClick = {navController.navigateUp() }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                }
 
+            )
+        }
+    ) {}
 }
