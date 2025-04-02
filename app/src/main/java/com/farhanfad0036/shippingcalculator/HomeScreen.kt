3db24a7b6e8c20424844bb71ee42541ce1bed602
Muhammad Farhan Fadlillah <ffadlillah18@gmail.com>
2025-04-02 15:19:32 +0700

    Membuat halaman utama

---
+++ app/src/main/java/com/farhanfad0036/shippingcalculator/HomeScreen.kt
@@ -0,0 +1,61 @@
+package com.farhanfad0036.shippingcalculator
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.farhanfad0036.shippingcalculator.navigation.Screen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun  HomeScreen(navController: NavController) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text("Shipping Calculator") }
+            )
+        }
+    ) { paddingValues ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_icon),
+                contentDescription = "App Logo",
+                modifier = Modifier.size(165.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Shipping Cost Calculator", style = MaterialTheme.typography.headlineMedium)
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(onClick = {navController.navigate(Screen.Input.route)}) {
+                Text("Start")
+            }
+        }
+    }
+
+
+}
