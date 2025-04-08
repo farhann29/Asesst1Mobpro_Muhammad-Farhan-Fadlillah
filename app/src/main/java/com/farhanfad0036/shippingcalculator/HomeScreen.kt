a2c6707a5120e76db83b0046a5ec9abc6b8e58ae
Muhammad Farhan Fadlillah <ffadlillah18@gmail.com>
2025-04-08 13:59:01 +0700

    membuat tulisan rata tengah

--- app/src/main/java/com/farhanfad0036/shippingcalculator/HomeScreen.kt
+++ app/src/main/java/com/farhanfad0036/shippingcalculator/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -56,8 +58,12 @@ fun  HomeScreen(navController: NavController) {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text(stringResource(R.string.desc_app), style = MaterialTheme.typography.bodyLarge)
-
+            Text(
+                text = stringResource(R.string.desc_app),
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
             Spacer(modifier = Modifier.height(32.dp))
 
             Button(onClick = {navController.navigate(Screen.Input.route)}) {
